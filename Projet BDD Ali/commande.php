f5aecaa0669dd20d109922c00af7aa5db822faff
Enzo1401 <154237934+Enzo1401@users.noreply.github.com>
2024-09-25 09:42:20 +0200

    Add files via upload

---
+++ Projet BDD Ali/commande.php
@@ -0,0 +1,55 @@
+<?php
+include("header.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commander un produit</title>
+
+    <link rel="stylesheet" href="style.css  ">
+</head>
+<body>
+
+    <form action="validation.php" method="post" class="form">
+        <div>
+            <label for="nom"></label>
+            <input type="text" name="nom" id="nom" placeholder="Entrez votre nom">
+        </div>
+        <div>
+            <label for="prenom"></label>
+            <input type="text" name="prenom" id="prenom" placeholder="Entrez votre prénom">
+        </div>
+        <div>
+            <label for="adresse"></label>
+            <input type="text" name="adresse" id="adresse" placeholder="Entrez votre adresse">
+        </div>
+        <div>
+            <label for="email"></label>
+            <input type="email" name="email" id="email" placeholder="Entrez votre adresse mail" required>
+        </div>
+     
+        <div>
+            <label for="produit">Sélectionnez un produit :</label>
+                <select name="produit" id="produit">
+                    <?php
+                    foreach ($produits as $produit) {
+                        echo '<option value="' . $produit['id_produit'] . '">' . $produit['nom'] . '</option>';
+                    }
+                    ?>
+                </select>
+            <br>
+        </div>
+     
+        <div>
+             <textarea name="message" placeholder="Ecrivez-nous"></textarea>
+        </div>
+        <div>
+            <button type="submit" class="button">Commander</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Projet BDD Ali/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>REDBOUL</title>
+</head>
+
+ 
+<body>
+   
+    <!--header-->
+    
+        <header class="header" id="mainHeader">
+            <div class="logo">
+            <a href="index.php"> <img src="image/redboul.jpeg" alt="logo de l'entreprise"></a>
+            </div>
+            <div class="logos"><a href="index.php"> <h1 class="no-decoration">REDBOUL</h1> </a></div>
+            <nav>
+                <ul class="color">
+                    <li><a href="commande.php" class="no-decoration">Commander</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <!--header-->
