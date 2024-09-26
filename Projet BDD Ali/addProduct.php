2c7b648c3db34b933aeb3dadcdd28962786ec638
Enzo1401 <enzomresse1@gmail.com>
2024-09-26 10:12:21 +0200

    style delete add

--- Projet BDD Ali/addProduct.php
+++ Projet BDD Ali/addProduct.php
@@ -20,9 +20,8 @@ include 'header.php';
     //créer un message de succès
     echo '<div style="text-align: center; margin-top: 100px;">';
     echo '<h2>Votre produit a bien été ajouté !</h2>';
-    echo '<a href="index.php" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;">Revenir à l\'accueil</a>';
+    echo '<a href="index.php" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #043464; color: white; text-decoration: none; border-radius: 5px;">Revenir à l\'accueil</a>';
     echo '</div>';
-    include 'footer.php';
 
     
 }
@@ -34,9 +33,6 @@ else
     echo '<h2>Une erreur est survenue lors de l\'ajout du produit.</h2>';
     echo '<a href="index.php" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #f44336; color: white; text-decoration: none; border-radius: 5px;">Revenir à l accueil</a>';
     echo '</div>';
-    include 'footer.php';
+    
 }
-
-
-
 ?>
