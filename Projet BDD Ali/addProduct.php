39bd341319dee753d0761935142b6f1a3d5df000
axelinho75 <153322643+axelinho75@users.noreply.github.com>
2024-09-26 07:56:46 +0000

    addProduct

--- Projet BDD Ali/addProduct.php
+++ Projet BDD Ali/addProduct.php
@@ -16,10 +16,27 @@ $addProduct = $bdd ->prepare('INSERT INTO produit(nom, prix, descriptions, image
 
 if($addProduct -> execute([$nom, $prix, $description, $imageName, $quantite]))
 {
-    //ne rien faire
+include 'header.php';
+    //créer un message de succès
+    echo '<div style="text-align: center; margin-top: 100px;">';
+    echo '<h2>Votre produit a bien été ajouté !</h2>';
+    echo '<a href="index.php" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;">Revenir à l\'accueil</a>';
+    echo '</div>';
+    include 'footer.php';
+
+    
 }
 else
 {
     //créer un message d'erreur
+    include 'header.php';
+    echo '<div style="text-align: center; margin-top: 20px;">';
+    echo '<h2>Une erreur est survenue lors de l\'ajout du produit.</h2>';
+    echo '<a href="index.php" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #f44336; color: white; text-decoration: none; border-radius: 5px;">Revenir à l accueil</a>';
+    echo '</div>';
+    include 'footer.php';
 }
+
+
+
 ?>
